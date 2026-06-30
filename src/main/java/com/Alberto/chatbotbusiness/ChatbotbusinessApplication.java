35ae6894df0396c0d3f3ca90fc6b072e93ba6bbb
AlbertoGonzález <albertogonzalezcorp@gmail.com>
2026-06-30 11:26:36 +0200

    Se inicializa el control de versiones del proyecto del chatbot, añadiendo todos los archivos del repositorio

---
+++ src/main/java/com/Alberto/chatbotbusiness/ChatbotbusinessApplication.java
@@ -0,0 +1,13 @@
+package com.Alberto.chatbotbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotbusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotbusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Alberto/chatbotbusiness/ChatbotbusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alberto.chatbotbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotbusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
